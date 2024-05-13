3584d75953ab209dd5ce1e088a56be4bb50297c0
chunji08 <chunji08@gmail.com>
2024-05-13 15:59:42 -0700

    roll back

--- test_app.py
+++ test_app.py
@@ -10,8 +10,7 @@ class TestSnippet(unittest.TestCase):
 
         print(f"Testing function - {check_posted_data.__name__} Set 1")
 
-        # data_list = [[{"x": 184, "y": 323}, "add", 200], [{"x": 143, "y": 0}, "division", 302], [{"x": 142}, "division", 301]]
-        data_list = []
+        data_list = [[{"x": 184, "y": 323}, "add", 200], [{"x": 143, "y": 0}, "division", 302], [{"x": 142}, "division", 301]]
         
         for data in data_list:
             result = check_posted_data(data[0], data[1])
@@ -20,17 +19,17 @@ class TestSnippet(unittest.TestCase):
 
             print(f"Test data - {data}")
 
-    # def test_check_posted_data_second_set(self):
-    #     print(f"Testing function - {check_posted_data.__name__} Set 2")
-    #     data_list = [[{"x": 184, "y": 323}, "add", 200], [{"x": 143, "y": 0}, "division", 302], [{"y": 143}, "division",
-    #                                                                                              301]]
+    def test_check_posted_data_second_set(self):
+        print(f"Testing function - {check_posted_data.__name__} Set 2")
+        data_list = [[{"x": 184, "y": 323}, "add", 200], [{"x": 143, "y": 0}, "division", 302], [{"y": 143}, "division",
+                                                                                                 301]]
 
-    #     for data in data_list:
-    #         result = check_posted_data(data[0], data[1])
+        for data in data_list:
+            result = check_posted_data(data[0], data[1])
 
-    #         self.assertEqual(result, data[2])
+            self.assertEqual(result, data[2])
 
-    #         print(f"Test data - {data}")
+            print(f"Test data - {data}")
 
 
 if __name__ == "__main__":
